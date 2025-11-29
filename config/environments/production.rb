1eb634c862ab20165216438ab9e376843d5d3b18
kadafs <kadafseshafe@gmail.com>
2025-11-29 07:19:15 +0100

    Update AWS SES configuration for aws-sdk-rails 5.x API

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,17 +119,11 @@ Rails.application.configure do
   else
     zone = ENV['AWS_S3_REGION']
 
-    creds = Aws::Credentials.new(
-      ENV['AWS_ACCESS_KEY_ID'],
-      ENV['AWS_SECRET_ACCESS_KEY']
-    )
-
-    Aws::Rails.add_action_mailer_delivery_method(
-      :ses,
-      credentials: creds,
-      region: zone
-    )
-
     config.action_mailer.delivery_method = :ses
+    config.action_mailer.ses_settings = {
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: zone
+    }
   end
 end
