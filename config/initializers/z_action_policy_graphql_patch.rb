6e4686fa951256ebc731a8bac37c6702393f51d8
kadafs <kadafseshafe@gmail.com>
2025-12-01 07:44:10 +0100

    Update action_policy-graphql patch to use prepend module pattern

--- config/initializers/z_action_policy_graphql_patch.rb
+++ config/initializers/z_action_policy_graphql_patch.rb
@@ -3,17 +3,22 @@
 # Patch action_policy-graphql to remove default_value from field options
 # GraphQL 1.13.x doesn't accept default_value as a keyword argument for fields
 # (it's only valid for arguments, not fields)
-# Using to_prepare ensures this runs after the gem is loaded
-Rails.application.config.to_prepare do
-  ActionPolicy::GraphQL::AuthorizedField.class_eval do
-    alias_method :original_initialize, :initialize unless method_defined?(:original_initialize)
+module ActionPolicyGraphQLPatch
+  def initialize(*args, **kwargs, &block)
+    # Remove default_value from kwargs if present (it's not valid for fields in GraphQL 1.13.x)
+    kwargs = kwargs.dup
+    kwargs.delete(:default_value) if kwargs.key?(:default_value)
+    super(*args, **kwargs, &block)
+  end
+end
 
-    def initialize(*args, **kwargs, &block)
-      # Remove default_value from kwargs if present (it's not valid for fields in GraphQL 1.13.x)
-      kwargs = kwargs.dup
-      kwargs.delete(:default_value) if kwargs.key?(:default_value)
-      
-      original_initialize(*args, **kwargs, &block)
+# Patch immediately if class is already loaded, otherwise use to_prepare
+if defined?(ActionPolicy::GraphQL::AuthorizedField)
+  ActionPolicy::GraphQL::AuthorizedField.prepend(ActionPolicyGraphQLPatch)
+else
+  Rails.application.config.to_prepare do
+    if defined?(ActionPolicy::GraphQL::AuthorizedField)
+      ActionPolicy::GraphQL::AuthorizedField.prepend(ActionPolicyGraphQLPatch) unless ActionPolicy::GraphQL::AuthorizedField < ActionPolicyGraphQLPatch
     end
   end
 end
