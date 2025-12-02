d4dfdc8d3d4214e98c97c0a3fa7fe9a0fb939faf
kadafs <kadafseshafe@gmail.com>
2025-12-02 07:37:30 +0100

    Update seeds to create ArticleSetting and run seeds in preDeployCommand

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,27 @@ domain = ENV['HOST'] || 'http://localhost:3000'
 
 AppPackagesCatalog.update_all unless Rails.env.test?
 
-app = App.create(
-  name: 'test app',
-  domain_url: domain
-)
+# Find or create the app
+app = App.find_or_create_by(name: 'test app') do |a|
+  a.domain_url = domain
+end
 
-Doorkeeper::Application.create(
-  name: 'authapp',
-  confidential: false
+# Update domain_url if it changed
+app.update(domain_url: domain) if app.domain_url != domain
+
+# Create ArticleSetting for the app if it doesn't exist
+unless app.article_settings.present?
+  # Extract subdomain from domain (remove protocol and path)
+  host = domain.gsub(/^https?:\/\//, '').split('/').first
+  subdomain = host.split('.').first if host.present?
+  
+  app.create_article_settings(
+    subdomain: subdomain || 'app',
+    domain: host || domain
+  )
+end
+
+Doorkeeper::Application.find_or_create_by(name: 'authapp') do |doorkeeper_app|
+  doorkeeper_app.confidential = false
   # redirect_uri: "#{domain}/callback"
-)
+end
